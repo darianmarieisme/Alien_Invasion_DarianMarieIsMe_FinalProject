ee8ccc05df893432810b2646b8ac3f03b95e6522
Darian Bruce <darianmariebruce@gmail.com>
2026-04-26 17:15:16 -0400

    added type hints and doc strings to hud module

--- game_stats.py
+++ game_stats.py
@@ -14,27 +14,36 @@ if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
 
 class GameStats():
+    '''This module tracks game stats such as score, lives, and level
+    it handles current score tracking, high score tracking,
+    and resetting the game between sessions'''
 
     def __init__(self, game: 'AlienInvasion') -> None:
-        self.game = game
+        '''This function initializes and sets all data for the game stats class
+        '''
+        self.game: AlienInvasion = game
         self.settings = game.settings
-        self.max_score = 0
+        self.max_score: int = 0
         self.init_saved_scores()
         self.reset_stats()
 
     def init_saved_scores(self) -> None:
+        '''loads the saved high score from a file or initializes it if
+        it is not available
+        '''
         self.path = self.settings.scores_file
         if self.path.exists() and self.path.stat().st_size > 0:
             contents = self.path.read_text()
             scores = json.loads(contents)
             self.hi_score = scores.get('hi_score', 0)
         else:
-            self.hi_score = 0
+            self.hi_score: int = 0
             self.save_scores()
             # save the file
 
     def save_scores(self) -> None:
-        scores = {
+        '''saves any current high score to a file'''
+        scores: dict[str, int] = {
             'hi_score': self.hi_score
         }
         contents = json.dumps(scores, indent=4)
@@ -45,11 +54,16 @@ class GameStats():
             print(f"File Not Found: {e}")
 
     def reset_stats(self) -> None:
+        '''this resets dynamic game statistics for a new game session
+        '''
         self.ships_left = self.settings.starting_ship_count
-        self.score = 0
-        self.level = 1
+        self.score: int = 0
+        self.level: int = 1
 
-    def update(self, collisions) -> None:
+    def update(self, collisions: dict[object, list[object]]) -> None:
+        '''This updates score related statistics based on collisions
+        Arguments: collisions(dict): mapping of collided sprites returned by
+        pygame.sprite.groupcollide()'''
         #update score
         self._update_score(collisions)
         
@@ -58,6 +72,7 @@ class GameStats():
         self._update_hi_score()
 
     def _update_max_score(self) -> None:
+        '''This updates the maximum score in the current session'''
         if self.score > self.max_score:
             self.max_score = self.score
             self.save_scores()
@@ -65,17 +80,24 @@ class GameStats():
       #  print(f'Max: {self.max_score}')
 
     def _update_hi_score(self) -> None:
+        '''This updates the all time high score if the current
+        score exceeds it'''
         if self.score > self.hi_score:
             self.hi_score = self.score
 
        # print(f'Hi: {self.max_score}')
 
 
-    def _update_score(self, collisions) -> None:
+    def _update_score(self, collisions: dict[object, list[object]]) -> None:
+        '''increases the score based on each alien destroyed
+        arguments: collsions (dict): mapping of collsions between bullets
+        and aliens
+        '''
         for alien in collisions.values():
             self.score += self.settings.alien_points
         # print(f'Basic: {self.score}')
 
     def update_level(self) -> None:
+        '''increases the current game level'''
         self.level += 1
         # print(self.level)
--- hud.py
+++ hud.py
@@ -6,34 +6,44 @@ This module deals with the HUD of the game
 '''
 
 import pygame.font
+from pygame import Surface, Rect
+import pygame
 #from typing import TYPE_CHECKING
 
 #if TYPE_CHECKING:
    # from alien_invasion import AlienInvasion
 
 class HUD:
-
+    '''This class creates and manages the HUD elements that
+    display player lives, score, high score, max score, and current level
+    '''
     def __init__(self, game) -> None:
+        '''This initalizes HUD elements and prepares visual components
+        Arguments: game (AlienInvasion): the main game instance providing access to screen,
+        settings, and game statistics'''
         self.game = game
         self.settings = game.settings
-        self.screen = game.screen
-        self.boundaries = game.screen.get_rect()
+        self.screen: Surface = game.screen
+        self.boundaries: Rect = game.screen.get_rect()
         self.game_stats = game.game_stats
-        self.font = pygame.font.Font(self.settings.font_file, 
+        self.font: pygame.font.Font = pygame.font.Font(self.settings.font_file, 
             self.settings.hud_font_size)
-        self.padding = 20
+        self.padding: int = 20
         self._setup_life_image()
         self.update_scores()
         self.update_level()
 
     def _setup_life_image(self) -> None:
-        self.life_image = pygame.image.load(self.settings.heart_file)
+        '''This module loads and scales the image used to represent remaining lives
+        '''
+        self.life_image: Surface = pygame.image.load(self.settings.heart_file)
         self.life_image = pygame.transform.scale(self.life_image, (
                 self.settings.heart_w, self.settings.heart_h
                 ))
-        self.life_rect = self.life_image.get_rect()
+        self.life_rect: Rect = self.life_image.get_rect()
 
     def update_scores(self) -> None:
+        '''This method updates the score related HUD elements'''
         self._update_max_score()
         self._update_score()
         self._update_hi_score()
@@ -41,45 +51,59 @@ class HUD:
         
 
     def _update_score(self) -> None:
-        self.score_str = f'Score: {self.game_stats.score:,.0f}'
-        self.score_image = self.font.render(self.score_str, True,
+        '''This method renders and positions the current score display
+        '''
+        self.score_str: str = f'Score: {self.game_stats.score:,.0f}'
+        self.score_image: Surface = self.font.render(self.score_str, True,
                 self.settings.text_color, None)
-        self.score_rect = self.score_image.get_rect()
+        self.score_rect: Rect = self.score_image.get_rect()
         self.score_rect.right = self.boundaries.right - self.padding
         self.score_rect.top = self.max_score_rect.bottom + (self.padding * 1.5)
 
     def _update_max_score(self) -> None:
-        self.max_score_str = f'Max-Score: {self.game_stats.max_score:,.0f}'
-        self.max_score_image = self.font.render(self.max_score_str, True,
+        '''This method renders and postions the max score display'''
+        self.max_score_str: str = f'Max-Score: {self.game_stats.max_score:,.0f}'
+        self.max_score_image: Surface = self.font.render(self.max_score_str, True,
                 self.settings.text_color, None)
-        self.max_score_rect = self.max_score_image.get_rect()
+        self.max_score_rect: Rect = self.max_score_image.get_rect()
         self.max_score_rect.right = self.boundaries.right - self.padding
         self.max_score_rect.top = self.padding
 
     def _update_hi_score(self) -> None:
-        self.hi_score_str = f'Hi-Score: {self.game_stats.hi_score:,.0f}'
-        self.hi_score_image = self.font.render(self.hi_score_str, True,
+        '''This method renders and positions the high score display
+        '''
+        self.hi_score_str: str = f'Hi-Score: {self.game_stats.hi_score:,.0f}'
+        self.hi_score_image: Surface = self.font.render(self.hi_score_str, True,
                 self.settings.text_color, None)
-        self.hi_score_rect = self.hi_score_image.get_rect()
+        self.hi_score_rect: Rect = self.hi_score_image.get_rect()
         self.hi_score_rect.right = self.boundaries.right - self.padding
         self.hi_score_rect.midtop = (self.boundaries.centerx, self.padding)
 
     def update_level(self) -> None:
-        self.level_str = f'Level: {self.game_stats.level:,.0f}'
-        self.level_image = self.font.render(self.level_str, True,
+        '''This method renders and positions the update level display
+        '''
+        self.level_str: str = f'Level: {self.game_stats.level:,.0f}'
+        self.level_image: Surface = self.font.render(self.level_str, True,
                 self.settings.text_color, None)
-        self.level_rect = self.level_image.get_rect()
+        self.level_rect: Rect = self.level_image.get_rect()
         self.level_rect.left = self.padding
         self.level_rect.top = self.life_rect.bottom + (self.padding * 1.5)
 
     def _draw_lives(self) -> None:
-        current_x = self.padding
-        current_y = self.padding
+        '''This method draws remaining lives as hearts on the screen
+        '''
+        current_x: int = self.padding
+        current_y: int = self.padding
         for _ in range(self.game_stats.ships_left):
             self.screen.blit(self.life_image, (current_x, current_y))
             current_x += self.life_rect.width + self.padding
 
-    def _draw_panel(self, rect, padding=10) -> None:
+    def _draw_panel(self, rect: Rect, padding: int = 10) -> None:
+        '''This method draws a background panel
+        Arguments: 
+        rect (rect): the rectangle of the UI element
+        padding (int): space added around the panel
+        '''
         panel_rect = pygame.Rect(
             rect.left - padding,
             rect.top - padding,
@@ -88,7 +112,12 @@ class HUD:
         )
         pygame.draw.rect(self.screen, (0, 0, 0), panel_rect)
 
-    def _draw_outlined_text(self, text, rect) -> None:
+    def _draw_outlined_text(self, text: str, rect: Rect) -> None:
+        '''This method draws text with a black outline
+        Arguments:
+        text(str): the text string to render
+        rect(rect): the position where the text should be drawn
+        '''
         outline = self.font.render(text, True, (0, 0, 0))
         main = self.font.render(text, True, self.settings.text_color)
         for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1),
@@ -97,6 +126,7 @@ class HUD:
         self.screen.blit(main, rect)
 
     def draw(self) -> None:
+        '''Draws the HUD elements onto the screen'''
         self._draw_panel(self.hi_score_rect)
         self.screen.blit(self.hi_score_image, self.hi_score_rect)
 
