8e504e65bbaa11f6a3d6451f19327c99b4d71495
Darian Bruce <darianmariebruce@gmail.com>
2026-04-26 13:48:08 -0400

    modified alien invasion file with button and HUD modules

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,6 +13,8 @@ from arsenal import Arsenal
 from game_stats import GameStats
 from alien_fleet import AlienFleet
 from time import sleep
+from button import Button
+from hud import HUD
 
 class AlienInvasion:
     """Main class that manages game initialization, game loop, and overall behavior
@@ -27,6 +29,8 @@ class AlienInvasion:
 
         self.screen: pygame.Surface = pygame.display.set_mode(
             (self.settings.screen_w, self.settings.screen_h))
+        self.game_stats = GameStats(self)
+        self.HUD = HUD(self)
         pygame.display.set_caption(self.settings.name)
 
         self.bg: pygame.Surface  = pygame.image.load(self.settings.bg_file)
@@ -46,8 +50,7 @@ class AlienInvasion:
         self.ship: Ship = Ship(self, Arsenal(self))
         self.alien_fleet: AlienFleet = AlienFleet(self)
         self.alien_fleet.create_fleet()
-        self.game_active: bool = True
-
+        self.game_active: bool = False
 
     def run_game(self) -> None:
         '''This method contains the main loop that runs the game logic'''
@@ -77,9 +80,14 @@ class AlienInvasion:
         if collisions:
             self.impact_sound.play()
             self.impact_sound.fadeout(500)
+            self.game_stats.update(collisions)
+            self.HUD.update_scores()
 
         if self.alien_fleet.check_destroyed_status():
             self._reset_level()
+            self.settings.increase_difficulty()
+            self.game_stats.update_level()
+            self.HUD.update_level()
 
     def _check_game_status(self):
         if self.game_stats.ships_left  > 0:
@@ -101,7 +109,22 @@ class AlienInvasion:
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
         self.alien_fleet.draw()
+        self.HUD.draw()
+
+        if not self.game_active:
+            self.play_button.draw()
+            pygame.mouse.set_visible(True)
+
         pygame.display.flip()
+
+    def restart_game(self) -> None:
+       self.settings.initialize_dynamic_settings
+       self.game_stats.reset_stats()
+       self.HUD.update_scores()
+       self._reset_level()
+       self.ship._center_ship()
+       self.game_active = True
+       pygame.mouse.set_visible(False)
 
 
     def _check_events(self) -> None:
@@ -115,6 +138,13 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self._check_button_clicked()
+
+    def _check_button_clicked(self) -> None:
+        mouse_pos = pygame.mouse.get_pos()
+        if self.play_button.check_clicked(mouse_pos):
+            self.restart_game
 
     def _check_keyup_events(self, event) -> None:
         '''this method responds to key release events and stops ship
